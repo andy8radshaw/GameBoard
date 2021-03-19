0862f86a2e695c8da5363429d654fdfaa8af2374
andy8radshaw <andy8radshaw@gmail.com>
2021-03-19 21:49:29 +0000

    added tests for get index, single, myprofile

--- test/api/user-tests/users.test.js
+++ test/api/user-tests/users.test.js
@@ -4,21 +4,56 @@ import request from 'supertest'
 import app from '../../../index.js'
 
 let token
+let userId
 
 describe('User -----------------------', () => {
-  describe('GET /profiles', () => {
 
-    before(async () => {
-      await request(app).post('/api/register')
-        .send({ 
-          email: 'test@email.com',
-          password: 'password',
-          passwordConfirmation: 'password'
-        })
+  before(async () => {
+    await request(app).post('/api/register')
+      .send({ 
+        email: 'test@email.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      })
+      .then((res) => {
+        token = res.body.token
+      })
+  })
+
+  describe('--- GET /myprofile (Logged in users profile)', () => {
+
+    it('Returns an object', async () => {
+      await request(app).get('/api/myprofile')
+        .set('Authorization', 'Bearer ' + token)
         .then((res) => {
-          token = res.body.token
+          const user = res.body
+          userId = user.id
+          expect(user).to.be.an('object')
         })
     })
+
+    it('Returns an object with the correct properties', async () => {
+      await request(app).get('/api/myprofile')
+        .set('Authorization', 'Bearer ' + token)
+        .then((res) => {
+          const user = res.body
+          expect(user).to.have.property('email')
+          expect(user).to.have.property('id')
+        })
+    })
+
+    it('Returns the correct email for test account', async () => {
+      await request(app).get('/api/myprofile')
+        .set('Authorization', 'Bearer ' + token)
+        .then((res) => {
+          const user = res.body
+          expect(user.email).to.equal('test@email.com')
+        })
+    })
+
+  })
+
+  describe('--- GET /profiles (userIndex)', () => {
 
     it('Returns an array', async () => {
       await request(app).get('/api/profiles')
@@ -30,7 +65,7 @@ describe('User -----------------------', () => {
         })
     })
 
-    it('Has the correct length', async () => {
+    it('Returns an array of the correct length', async () => {
       await request(app).get('/api/profiles')
         .set('Authorization', 'Bearer ' + token)
         .then((res) => {
@@ -40,7 +75,7 @@ describe('User -----------------------', () => {
         })
     })
 
-    it('Has the correct properties', async () => {
+    it('Returns objects with the correct properties', async () => {
       await request(app).get('/api/profiles')
         .set('Authorization', 'Bearer ' + token)
         .then((res) => {
@@ -49,6 +84,17 @@ describe('User -----------------------', () => {
           expect(user).to.have.property('id')
         })
     })
-    
+  })
+
+  describe('--- GET /profiles/:id (a single user) ', () => {
+
+    it('Returns an object', async () => {
+      await request(app).get(`/api/profiles/${userId}`)
+        .set('Authorization', 'Bearer ' + token)
+        .then((res) => {
+          const user = res.body
+          expect(user).to.be.an('object')
+        })
+    })
   })
 }) 
